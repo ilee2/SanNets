1b0be27e80492ef5eea46ea3891c6dad33bbfe1b
miutron <miutron@tests-MacBook-Pro.local>
2017-06-19 13:55:09 -0700

    new changes

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -5,16 +5,17 @@ class ContactFormsController < ApplicationController
 
   def create
   		#render text: params.inspect
+  		@contact = ContactForm.new
   		@stuff = ContactForm.new(params.require(:contact_form).permit(:name, :email, :mesage))
 
   	if @stuff.save
-  		flash[:success] = 'you message was sent'
+  		flash.now[:success] = 'Your message was sent.'
   		ContactMailer.new_contact(@stuff).deliver_now
+  		render 'new'
   	else
   		render 'new'
   	end		
-
-
- 	
   end	
+
+	
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,6 +3,6 @@ class ContactMailer < ApplicationMailer
 	def new_contact(contact)
     @thing = contact
 
-    mail to: 'miulee@ucdavis.edu', subject: "New Message was posted"
+    mail to: 'azizkatawazai@gmail.com', subject: "New Message was posted"
   end
 end
