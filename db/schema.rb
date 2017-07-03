7a2307dd1f2b2f3020d01e90141e15e89e5db6d9
miutron <miutron@tests-MacBook-Pro.local>
2017-07-02 20:13:21 -0700

    chaning the whole page

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619163737) do
+ActiveRecord::Schema.define(version: 20170620043330) do
 
   create_table "contact_forms", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.text     "mesage"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.text     "background"
+    t.integer  "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
